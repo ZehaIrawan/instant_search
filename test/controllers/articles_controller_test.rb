d29c8816d2557d0b5e8b72990f16759905d933d6
Zeha <zeha.irawan@yicom.com>
2024-01-04 05:05:29 +0800

    Fix controller naming convention

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class ArticleControllerTest < ActionDispatch::IntegrationTest
+class ArticlesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get article_index_url
+    get articles_index_url
     assert_response :success
   end
 end
