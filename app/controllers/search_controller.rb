102cafd8eada0981d22087c8cbc851eafc1fbb42
Zeha <zeha.irawan@yicom.com>
2024-01-04 13:59:01 +0800

    working form with js

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,13 @@ class SearchController < ApplicationController
     query = params[:query].presence
     if query.present?
       @results = Article.where("LOWER(title) LIKE ?", "%#{query.downcase}%")
-      print("@results: #{@results}")
     else
       @results = []
+    end
+
+    respond_to do |format|
+      format.html # Render the HTML by default
+      format.js   # Render JavaScript for AJAX requests
     end
   end
 end
