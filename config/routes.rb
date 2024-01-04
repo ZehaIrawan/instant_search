efa3781e9abd348c80e4064a41d5e939f691126b
Zeha <zeha.irawan@yicom.com>
2024-01-04 16:30:55 +0800

    setup analytics and searchlog

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'analytics/index'
   get 'search', to: 'search#search'
   resources :articles
+  resources :analytics
   root 'search#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_03_203729) do
+ActiveRecord::Schema.define(version: 2024_01_04_082517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2024_01_03_203729) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "search_logs", force: :cascade do |t|
+    t.string "query"
+    t.string "ip_address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
