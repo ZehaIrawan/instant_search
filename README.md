1e29ca4ee260c25dd4dd54eb292858555063f0e5
Zeha <zeha.irawan@yicom.com>
2024-01-05 19:37:00 +0800

    setup dev sidekiq

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 # Handling scaling with sidekiq
 Without sidekiq the search method in controller with wait until the creation of SearchLog db entry, which will slow down the app.
 
-With sidekiq we can put it in the background task and make it faster.
+With sidekiq we can put it in the background task and make it faster as it's using Redis as a job management store to process thousands of jobs per second.
 
 ## Bug
 [x] Search doesn't work if navigate to analytics then come back
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,7 @@ class SearchController < ApplicationController
     if query.present?
       @results = Article.where("LOWER(title) LIKE ?", "%#{query.downcase}%")
 
-       create_search_log(query)
+      create_search_log_async(query)
     else
       @results = []
     end
@@ -20,18 +20,8 @@ class SearchController < ApplicationController
 
   private
 
-  def create_search_log(query)
+  def create_search_log_async(query)
     user_ip = request.remote_ip
-    previous_query = SearchLog.where(ip_address: user_ip).order(created_at: :desc).pluck(:query).first
-
-    if previous_query.present? && query.downcase.include?(previous_query.downcase)
-      # If the new query includes the previous one, remove the previous log
-      SearchLog.where(ip_address: user_ip, query: previous_query).delete_all
-      summarized_query = query
-    else
-      summarized_query = query
-    end
-
-    SearchLog.create(query: summarized_query, ip_address: user_ip)
+    SearchLogWorker.perform_async(query, user_ip) 
   end
 end
