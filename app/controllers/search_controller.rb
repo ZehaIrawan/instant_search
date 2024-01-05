968d91268635e5e1c3a9e976e64049d168d93b6b
Zeha <zeha.irawan@yicom.com>
2024-01-05 14:17:14 +0800

    delete previous record if included in new one

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -22,19 +22,17 @@ class SearchController < ApplicationController
   private
 
   def create_search_log(query)
-  user_ip = request.remote_ip
-  previous_query = SearchLog.where(ip_address: user_ip).order(created_at: :desc).pluck(:query).first
+    user_ip = request.remote_ip
+    previous_query = SearchLog.where(ip_address: user_ip).order(created_at: :desc).pluck(:query).first
 
-  if previous_query.present?
-    if query.downcase.include?(previous_query.downcase)
+    if previous_query.present? && query.downcase.include?(previous_query.downcase)
+      # If the new query includes the previous one, remove the previous log
+      SearchLog.where(ip_address: user_ip, query: previous_query).delete_all
       summarized_query = query
     else
-      summarized_query = "#{previous_query}, #{query}"
+      summarized_query = query
     end
-  else
-    summarized_query = query
-  end
 
-  SearchLog.create(query: summarized_query, ip_address: user_ip)
+    SearchLog.create(query: summarized_query, ip_address: user_ip)
   end
 end
