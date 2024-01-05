1f1ec489fc3dee5a5c6e8328ae7e8681ee61c1f8
Zeha <zeha.irawan@yicom.com>
2024-01-05 19:45:30 +0800

    setup env

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,7 @@
-require 'sidekiq'
-
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' } 
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' }
 end
