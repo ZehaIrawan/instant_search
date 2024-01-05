28c472d65bf486184ebbeb93acf54a51b1d2b061
Zeha <zeha.irawan@yicom.com>
2024-01-05 21:34:47 +0800

    setup trends

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,5 +1,8 @@
 class AnalyticsController < ApplicationController
   def index
     @search_logs = SearchLog.all
+    @trends =   trends = SearchLog.select('query, COUNT(DISTINCT ip_address) AS user_count')
+                     .group(:query)
+                     .order('user_count DESC')
   end
 end
